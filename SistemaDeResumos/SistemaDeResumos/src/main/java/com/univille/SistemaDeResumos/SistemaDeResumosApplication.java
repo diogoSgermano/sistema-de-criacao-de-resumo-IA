17422bc8d20b15839dae705abac234541529ac82
Diogo Soares Germano <diogogermano28@gmail.com>
2025-11-12 19:37:41 -0300

    Create SistemaDeResumos
    Co-Authored-By: Leticia <180476917+leticiascg@users.noreply.github.com>

---
+++ SistemaDeResumos/SistemaDeResumos/src/main/java/com/univille/SistemaDeResumos/SistemaDeResumosApplication.java
@@ -0,0 +1,13 @@
+package com.univille.SistemaDeResumos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeResumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeResumosApplication.class, args);
+	}
+
+}
---
+++ SistemaDeResumos/SistemaDeResumos/src/test/java/com/univille/SistemaDeResumos/SistemaDeResumosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univille.SistemaDeResumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeResumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
